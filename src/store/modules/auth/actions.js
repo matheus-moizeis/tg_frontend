78229bc05cbd84822448299b8d9db459d21d47da
Matheus Moizeis <matheusmoizeis@outlook.com>
2022-04-30 08:25:56 -0300

    fix: configuracao do estado da aplicacao auth/actions

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -2,11 +2,10 @@ import { api } from 'boot/axios'
 
 export async function createUser ({ commit }, payload) {
   try {
-    const { status, data } = await api.post('/users', payload).response
-    commit('setUser', {status, data})
-  }
-  catch (error) {
+    const { status, data } = await (api.post('/users', payload))
+    commit('setUser', { status, data })
+  } catch (error) {
     const { status, data } = error.response
-    commit('setUser', errorCreate)
+    commit('setUser', { status, data })
   }
 }
