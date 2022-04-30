e096b946dc2f24d9c5bf7d31d2d70543eea114bc
Matheus Moizeis <matheusmoizeis@outlook.com>
2022-04-30 10:58:51 -0300

    feature: ajuste tela de login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -6,7 +6,7 @@
         <q-form ref="loginForm" @submit="onSubmit" class="q-gutter-md">
           <q-input
             filled
-            v-model="email"
+            v-model="formData.email"
             label="Email *"
             lazy-rules
             type="email"
@@ -19,7 +19,7 @@
           <q-input
             filled
             type="password"
-            v-model="password"
+            v-model="formData.password"
             label="Senha *"
             lazy-rules
             :rules="passwordRules"
@@ -39,19 +39,69 @@
 </template>
 
 <script>
+import { api } from 'boot/axios'
+import { QSpinnerGears } from 'quasar'
+
 export default {
   name: 'LoginPage',
   data () {
     return {
-      email: null,
-      password: null
+      formData: {
+        email: null,
+        password: null
+      }
     }
   },
 
   methods: {
-    onSubmit () {
-      alert('ok')
+    async onSubmit () {
+      api.post('/tokens', {
+        email: this.formData.email,
+        password: this.formData.password
+      })
+        .then(response => {
+          this.$q.notify({
+            spinner: QSpinnerGears,
+            timeout: 1000,
+            message: 'Carregando...',
+            position: 'center'
+          })
+          this.setToken(response.data.token)
+          this.$router.push({ name: 'Ticket' })
+        })
+        .catch(error => {
+          this.$q.notify({
+            type: 'negative',
+            position: 'top',
+            avatar: true,
+            timeout: 500,
+            message: error.response.data.errors
+          })
+          this.onReset()
+        })
+    },
+
+    async setToken (token) {
+      try {
+        localStorage.clear()
+        return await localStorage.setItem('token', 'Bearer ' + token)
+      } catch (error) {
+        alert(error)
+      }
+    },
+
+    async onReset () {
+      await this.resetForm()
+      this.$refs.loginForm.resetValidation()
+    },
+
+    async resetForm () {
+      this.formData = {
+        email: '',
+        password: ''
+      }
     }
+
   },
 
   computed: {
--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -4,8 +4,11 @@ export async function createUser ({ commit }, payload) {
   try {
     const { status, data } = await (api.post('/users', payload))
     commit('setUser', { status, data })
+    if (status === 200) {
+      this.$router.push({ name: 'Login' })
+    }
   } catch (error) {
-    const { status, data } = error.response
+    const { status, data } = error
     commit('setUser', { status, data })
   }
 }
